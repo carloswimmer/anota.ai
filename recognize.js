5f81c90c882b25d68ddabb609f42935d02046cbe
carloswimmer <coiote777@gmail.com>
2019-07-12 18:30:15 -0300

    Checking if browser has voice recognition

--- recognize.js
+++ recognize.js
@@ -1,40 +1,59 @@
-var alertSuccess = document.querySelector('.alert')
-alertSuccess.style.display = 'none'
+window.SpeechRecognition = window.webkitSpeechRecognition ||
+  window.SpeechRecognition
+var notification = document.querySelector('.alert')
 var recognizing
-var recognition = new webkitSpeechRecognition()
-recognition.continuous = true
-reset()
-recognition.onend = reset()
+var recognition = {}
 
-recognition.onresult = function (event) {
-  for (var i = event.resultIndex; i < event.results.length; ++i) {
-    if (event.results[i].isFinal) {
-      display.value += event.results[i][0].transcript
+window.onload = function () {
+  if ('SpeechRecognition' in window && 
+    typeof window.SpeechRecognition === 'function') {
+    letsTakeNote()
+    return
+  }
+  notification.classList.toggle('alert-danger')
+  notification.style.opacity = '1'
+  notification.innerHTML = 'Esse navegador não tem reconhecimento de voz'
+  var controls = document.querySelectorAll('button')
+  controls.forEach(item => item.setAttribute('disabled', true))
+}
+
+function letsTakeNote () {
+  recognition = new window.SpeechRecognition()
+  recognition.continuous = true
+  reset()
+  
+  recognition.onend = reset()
+  
+  recognition.onresult = function (event) {
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        display.value += event.results[i][0].transcript
+      }
     }
   }
 }
 
-function reset() {
+function reset () {
   recognizing = false
   button.innerHTML = '<i class="fas fa-microphone fa-lg"></i>'
   button.classList.toggle('btn-danger')
 }
 
-function toggleStartStop() {
+function toggleStartStop () {
   if (recognizing) {
     recognition.stop()
     reset()
   } else {
-    recognition.start()
+    recognition.start ()
     recognizing = true
     button.innerHTML = '<i class="fas fa-microphone-slash fa-lg"></i>'
     button.classList.toggle('btn-danger')
   }
 }
 
-function clearDisplay() {
+function clearDisplay () {
   display.value = ''
-  alertSuccess.style.display = 'none'
+  notification.style.opacity = '0'
 }
 
 function copy () {
@@ -42,7 +61,7 @@ function copy () {
   element =  hideElement(element)
   includeSelectCopy(element)
   destroyElement(element)
-  alertSuccess.style.display = 'block'
+  notification.style.opacity = '1'
 }
 
 function buildElement (statement) {
